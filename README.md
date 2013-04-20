79e56c5f8da4f4f88114e1a45bce0d2fedab28b6
Koren <koren@pukogames.com>
2013-04-20 23:11:19 +0200

    Add base freeglut window

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ opengl-b-splines
 ================
 
 Learning B-Splines with OpenGL
+
+sudo apt-get install freeglut3-dev libxmu-dev libxi-dev
--- main.cpp
+++ main.cpp
@@ -1,4 +1,43 @@
-int main(int argc, char** argv){
+#include <GL/freeglut.h>
 
-	return 0;
+void keyboard(unsigned char key, int x, int y);
+void display(void);
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    int windowID = glutCreateWindow("BSplines - OpenGL");
+    glutKeyboardFunc(&keyboard);
+    glutDisplayFunc(&display);
+    glutMainLoop();
+    glutDestroyWindow(windowID);
+    glutExit();
+    return EXIT_SUCCESS;
+}
+
+
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case '\x1B':
+            exit(EXIT_SUCCESS);
+            break;
+    }
+}
+
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glVertex2f( 0.5f,  0.5f);
+        glVertex2f(-0.5f,  0.5f);
+    glEnd();
+
+    glFlush();
 }
