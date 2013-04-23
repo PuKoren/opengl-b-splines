d17d31ed9b43b52f9c71c252da1ae8ac744bf42b
Koren <koren@pukogames.com>
2013-04-23 23:25:20 +0200

    I don't know how to use git

---
+++ bin/CMakeFiles/BSplines.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/koren/Dev/c++/OpenGL/opengl-b-splines/Application.cpp" "/home/koren/Dev/c++/OpenGL/opengl-b-splines/bin/CMakeFiles/BSplines.dir/Application.cpp.o"
+  "/home/koren/Dev/c++/OpenGL/opengl-b-splines/main.cpp" "/home/koren/Dev/c++/OpenGL/opengl-b-splines/bin/CMakeFiles/BSplines.dir/main.cpp.o"
+  "/home/koren/Dev/c++/OpenGL/opengl-b-splines/maths/Vector2.cpp" "/home/koren/Dev/c++/OpenGL/opengl-b-splines/bin/CMakeFiles/BSplines.dir/maths/Vector2.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ bin/CMakeFiles/BSplines.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/BSplines.dir/maths/Vector2.cpp.o"
+  "CMakeFiles/BSplines.dir/Application.cpp.o"
+  "CMakeFiles/BSplines.dir/main.cpp.o"
+  "BSplines.pdb"
+  "BSplines"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/BSplines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
